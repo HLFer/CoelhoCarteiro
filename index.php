336f49bac953f3c339a7046c4b3cfcec0d345662
Henrique Fernandes <henry1991.contato@gmail.com>
2019-01-16 09:12:38 -0200

    Commit inicial - Estudo sobre RabbitMQ
    Exemplo de Framework Mensageiro utilizando RabbitMQ

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+echo '<pre><div align="center">';
+echo 'Coelho Carteiro - Framework para gerenciamento de mensagens.';
+echo '</div></pre>';
+
+require_once('sender.php');
+require_once('receiver.php');
